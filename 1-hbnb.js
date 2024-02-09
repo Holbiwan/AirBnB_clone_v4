9e4cdd2a960bd1e0fae47899e899e806a82f2cbc
Holbiwan <sabrinapapeau@gmail.com>
2024-02-09 12:06:21 +0100

    Fix code syntax

--- 1-hbnb.js
+++ 1-hbnb.js
@@ -1,26 +1,24 @@
-$(function () {
-    const entertainment = [];
-    $('.popover input').change(function (e) {
-      const { target } = e;
-      const name = target.getAttribute('data-name');
-      const id = target.getAttribute('data-id');
-  
-      const isChecked = $(`.popover input[data-id="${id}"]`).is(':checked');
-  
-      if (isChecked) {
-        entertainment.push(name);
-      } else {
-        const index = entertainment.indexOf(name);
-        if (index > -1) entertainment.splice(index, 1);
-      }
-  
-      if (entertainment.length > 0) {
-        const newText = entertainment.join(', ');
-        (newText.length >= 37)
-          ? $('.amenities h4').text(`${newText.slice(0, 37)}...`)
-          : $('.amenities h4').text(newText);
-      } else {
-        $('.amenities h4').html('&nbsp;');
-      }
+$(document).ready(function() {
+    let amenitiesChecked = {};
+
+    $('.amenities input[type="checkbox"]').change(function() {
+        if (this.checked) {
+            amenitiesChecked[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenitiesChecked[$(this).data('id')];
+        }
+
+        let selectedAmenities = Object.values(amenitiesChecked).join(', ');
+        const maxLength = 37;
+
+        if (selectedAmenities.length > maxLength) {
+            selectedAmenities = selectedAmenities.substring(0, maxLength) + '...';
+        }
+
+        if (selectedAmenities.length === 0) {
+            selectedAmenities = "&#160;";
+        }
+
+        $('.amenities h4').html(selectedAmenities);
     });
-  });
+});
