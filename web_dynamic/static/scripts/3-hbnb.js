fbcb3b6ba6085755a4d73003f6179dfd5e5533fd
HOLBIWAN (Saby) <sabrinapapeau@gmail.com>
2024-02-09 11:49:37 +0100

    Update 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,62 +1,64 @@
-$(function () {
-    const entertainment = [];
-    $('.popover input').change(function (e) {
-      const { target } = e;
-      const name = target.getAttribute('data-name');
-      const id = target.getAttribute('data-id');
-  
-      const isChecked = $(`.popover input[data-id="${id}"]`).is(':checked');
-  
-      if (isChecked) {
-        entertainment.push(name);
+$(document).ready(function() {
+  let amenitiesChecked = {};
+
+  $('.amenities input[type="checkbox"]').change(function() {
+      if (this.checked) {
+          amenitiesChecked[$(this).data('id')] = $(this).data('name');
       } else {
-        const index = entertainment.indexOf(name);
-        if (index > -1) entertainment.splice(index, 1);
+          delete amenitiesChecked[$(this).data('id')];
       }
-  
-      if (entertainment.length > 0) {
-        const newText = entertainment.join(', ');
-        newText.length >= 37
-          ? $('.amenities h4').text(`${newText.slice(0, 37)}...`)
-          : $('.amenities h4').text(newText);
-      } else {
-        $('.amenities h4').html('&nbsp;');
+
+      // Convertir les valeurs sélectionnées en chaîne et limiter le nombre de caractères
+      let selectedAmenities = Object.values(amenitiesChecked).join(', ');
+      const maxLength = 37; // Définir la limite de caractères ici
+
+      // Vérifier si la longueur dépasse la limite
+      if (selectedAmenities.length > maxLength) {
+          selectedAmenities = selectedAmenities.substring(0, maxLength) + '...';
       }
-    });
-    $.ajax({
-      type: 'GET',
-      url: 'http://127.0.0.1:5001/api/v1/status/',
-      success: function (data) {
-        data.status === 'OK'
-          ? $('div#api_status').addClass('available')
-          : $('div#api_status').removeClass('available');
-      },
-      error: function () {
-        $('div#api_status').removeClass('available');
+
+      // Utiliser un espace non-sécable si aucune commodité n'est sélectionnée
+      if (selectedAmenities.length === 0) {
+        selectedAmenities = "&#160;"; // Espace non-sécable HTML
       }
-    });
-    /* Task-4 */
-    $.ajax({
-      type: 'POST',
-      url: 'http://127.0.0.1:5001/api/v1/places_search/',
-      data: '{}',
-      contentType: 'application/json',
-      success: function (data) {
-        for (const e of data) {
-          $('section.places').append(`
-          <article>
-            <div class="title_box">
-              <h2>${e.name}</h2>
-              <div class="price_by_night">${e.price_by_night}</div>
-            </div>
-            <div class="information">
-              <div class="max_guest">${e.max_guest} Guest${e.max_guest !== 1 ? 's' : ''}</div>
-              <div class="number_rooms">${e.number_rooms} Bedroom${e.number_rooms !== 1 ? 's' : ''}</div>
-              <div class="number_bathrooms">${e.number_bathrooms} Bathroom${e.number_bathrooms !== 1 ? 's' : ''}</div>
-            </div>
-          </article>`);
-        }
-      },
-      dataType: 'json'
-    });
+
+      $('.amenities h4').html(selectedAmenities);
   });
+  
+  $.get('http://localhost:5001/api/v1/status/', function(data) {
+    // Vérifier si le statut est "OK"
+    if (data.status === 'OK') {
+      // Ajouter la classe "available" à l'élément div#api_status
+      $('div#api_status').addClass('available');
+    } else {
+      // Retirer la classe "available" de l'élément div#api_status
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function(places) {
+      places.forEach(function(place) {
+        const article = `<article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`;
+        $('.places').append(article);
+      });
+    }
+  });
+});
