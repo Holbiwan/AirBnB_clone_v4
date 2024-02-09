5ba7aeb9ed5b501a9cd8e57f8ccd31fb38b4c74d
Holbiwan <sabrinapapeau@gmail.com>
2024-02-09 12:08:09 +0100

    Fix code syntax

--- 2-hbnb.js
+++ 2-hbnb.js
@@ -1,39 +1,32 @@
-$(function () {
-  const entertainment = [];
-  $('.popover input').change(function (e) {
-    const { target } = e;
-    const name = target.getAttribute('data-name');
-    const id = target.getAttribute('data-id');
+$(document).ready(function() {
+  let amenitiesChecked = {};
 
-    const isChecked = $(`.popover input[data-id="${id}"]`).is(':checked');
+  $('.amenities input[type="checkbox"]').change(function() {
+      if (this.checked) {
+          amenitiesChecked[$(this).data('id')] = $(this).data('name');
+      } else {
+          delete amenitiesChecked[$(this).data('id')];
+      }
 
-    if (isChecked) {
-      entertainment.push(name);
-    } else {
-      const index = entertainment.indexOf(name);
-      if (index > -1) entertainment.splice(index, 1);
-    }
+      let selectedAmenities = Object.values(amenitiesChecked).join(', ');
+      const maxLength = 37;
 
-    if (entertainment.length > 0) {
-      const newText = entertainment.join(', ');
-      (newText.length >= 37)
-        ? $('.amenities h4').text(`${newText.slice(0, 37)}...`)
-        : $('.amenities h4').text(newText);
-    } else {
-      $('.amenities h4').html('&nbsp;');
-    }
+      if (selectedAmenities.length > maxLength) {
+          selectedAmenities = selectedAmenities.substring(0, maxLength) + '...';
+      }
+
+      if (selectedAmenities.length === 0) {
+       selectedAmenities = "&#160;";
+      }
+
+      $('.amenities h4').html(selectedAmenities);
   });
-  $.ajax({
-    type: 'GET',
-    url: 'http://127.0.0.1:5001/api/v1/status/',
-    success: function (data) {
-      (data.status === 'OK')
-        ? $('div#api_status').addClass('available')
-        : $('div#api_status').removeClass('available');
-    },
-    error: function () {
+  
+  $.get('http://localhost:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
       $('div#api_status').removeClass('available');
     }
   });
-}
-);
+});
